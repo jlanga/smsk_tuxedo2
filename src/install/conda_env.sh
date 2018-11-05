012bc12e2ef51ed01e5cd1f9176fda6413eab483
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-11-05 11:30:30 +0100

    Linting

--- src/install/conda_env.sh
+++ src/install/conda_env.sh
@@ -2,5 +2,5 @@ conda config --add channels conda-forge
 conda config --add channels defaults
 conda config --add channels r
 conda config --add channels bioconda
-conda env create --name smsk_tuxedo2 --file environment.yml
+conda env create --name smsk_tuxedo2 --file .travis_env.yml
 conda clean --all --yes
